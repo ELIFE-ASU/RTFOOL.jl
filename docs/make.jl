6605f2ecb6e6dfd89195497b5eba80956ccd2500
Douglas G. Moore <doug@dglmoore.com>
2018-05-13 23:13:43 +0000

    Setup documentation

---
+++ docs/make.jl
@@ -0,0 +1,9 @@
+using Documenter, RTFOOL
+
+makedocs(
+    format = :html,
+    sitename = "RTFOOL",
+    pages = [
+        "index.md",
+    ]
+)
---
+++ docs/src/index.md
@@ -0,0 +1,7 @@
+# RTFOOL - A Resource Theory for the Origin of Life
+
+```@meta
+CurrentModule = RTFOOL
+DocTestSetup = :(using RTFOOL)
+```
+
