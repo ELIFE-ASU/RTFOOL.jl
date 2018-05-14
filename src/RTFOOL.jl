c6eb97bcf0ac767f5e0dc5e506181507da49a41f
Douglas G. Moore <doug@dglmoore.com>
2018-05-14 17:10:04 +0000

    Implement the boltzmann distribution function
    This function constructs a probability array respresenting a
    Boltzmann distribution over a given collection of energy states

--- src/RTFOOL.jl
+++ src/RTFOOL.jl
@@ -1,10 +1,21 @@
 module RTFOOL
 
 """
-    boltzmann(β::Float64, E::Float64)
+    boltzmann(β, E::Number)
 
-Compute the Boltzmann factor for inverse temperature β and energy E.
+Compute the Boltzmann factor for inverse temperature `β` and energy `E`.
+
+    boltzmann(β, E)
+
+Construct the Boltzmann distributions with inverse temperature `β` over the energy states
+`E`.
 """
-boltzmann(β::Float64, E::Float64) = exp(-β*E)
+boltzmann(β, E::Number) = exp(-β*E)
+
+function boltzmann(β, E)
+    factors = boltzmann.(β, E)
+    partition = sum(factors)
+    factors ./ partition
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,12 @@ end
     @test RTFOOL.boltzmann(1.0, 1.0) == e^-1.00
     @test RTFOOL.boltzmann(1.0, 2.0) == e^-2.00
     @test RTFOOL.boltzmann(0.5, 1.5) == e^-0.75
+
+    @test RTFOOL.boltzmann(1.0, [1.0]) == [1.0]
+    @test RTFOOL.boltzmann(1.0, [2.0]) == [1.0]
+    @test RTFOOL.boltzmann(1.0, [1.0, 1.0]) == [0.5, 0.5]
+    @test RTFOOL.boltzmann(1.0, [1.0, 2.0]) == [e/(1+e), 1/(1+e)]
+    @test RTFOOL.boltzmann(0.5, [0.5, 1.5]) ≈ [e/(e + √e), √e/(e + √e)]
+    @test RTFOOL.boltzmann(1/3, [1, 2, 3]) ==
+        [e^-(1/3), e^-(2/3), e^-1] / (e^-(1/3) + e^-(2/3) + e^-1)
 end
