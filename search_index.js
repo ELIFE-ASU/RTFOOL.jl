81c7aa6341ec1cb9736899e27aa1c72f7a670e41
Douglas G. Moore <doug@dglmoore.com>
2018-05-14 00:05:15 +0000

    Initial documentation

---
+++ search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "RTFOOL - A Resource Theory for the Origin of Life",
+    "title": "RTFOOL - A Resource Theory for the Origin of Life",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#RTFOOL-A-Resource-Theory-for-the-Origin-of-Life-1",
+    "page": "RTFOOL - A Resource Theory for the Origin of Life",
+    "title": "RTFOOL - A Resource Theory for the Origin of Life",
+    "category": "section",
+    "text": "CurrentModule = RTFOOL\nDocTestSetup = :(using RTFOOL)"
+},
+
+]}
