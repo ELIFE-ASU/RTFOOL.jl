916a48120c31a51c800e23d6c54f53313c0c127c
Douglas G. Moore <doug@dglmoore.com>
2018-06-09 22:45:59 +0000

    Dispense with the Resource struct

--- src/RTFOOL.jl
+++ src/RTFOOL.jl
@@ -30,58 +30,6 @@ function boltzmann(β, E, deg)
     factors ./ partition
 end
 
-"""
-    Resource(p, H)
-
-Construct a resource with state (probability array) `p` and Hamiltonian (energy array) `H`.
-
-    Resource(β, H)
-
-Construct a resource with a Boltzmann distribution (at inverse temperature `β`) over the
-Hamiltonian `H`.
-
-    Resource(i, H)
-
-Construct a pure-state resource over the Hamiltonian `H`
-
-```jldoctest
-julia> Resource([0.1, 0.9], [1, 2])
-RTFOOL.Resource([0.1, 0.9], [1.0, 2.0])
-
-julia> Resource(0.5, [1, 2])
-RTFOOL.Resource([0.622459, 0.377541], [1.0, 2.0])
-
-julia> Resource(1, [1, 2])
-RTFOOL.Resource([1.0, 0.0], [1.0, 2.0])
-
-julia> Resource(3, [2, 3, 3, 4])
-RTFOOL.Resource([0.0, 0.0, 1.0, 0.0], [2.0, 3.0, 3.0, 4.0])
-```
-"""
-struct Resource
-    p::Vector{Float64}
-    H::Vector{Float64}
-
-    Resource(p, H) = if !(sum(p) ≈ 1.0)
-        throw(ArgumentError("probability array is not normalized"))
-    elseif size(p) != size(H)
-        throw(DimensionMismatch("size(p) != size(H)"))
-    else
-        new(p, H)
-    end
-end
-Resource(β::Float64, H) = Resource(boltzmann(β, H), H)
-Resource(i::Int, H) = let p = zeros(size(H))
-    p[i] = 1.0
-    Resource(p, H)
-end
-
-Base.size(r::Resource) = size(r.p)
-
-Base.length(r::Resource) = length(r.p)
-
-Base.isapprox(x::Resource, y::Resource) = x.p ≈ y.p && x.H ≈ y.H
-
 struct StateSpace{N}
     basis :: Vector{NTuple{N, Int}}
     energy :: Vector{Float64}
--- test/runtests.jl
+++ test/runtests.jl
@@ -30,49 +30,6 @@ entropy(p) = -dot(p, map(p -> (p != zero(p)) ? log(p) : 0.0, p))
 	[e^-(1/3), 2e^-(2/3), 3e^-1] / (e^-(1/3) + 2e^-(2/3) + 3e^-1)
 end
 
-@testset "Resources" begin
-    @test_throws MethodError Resource([], [])
-    @test_throws ArgumentError Resource([0.5], [0.0])
-    @test_throws ArgumentError Resource([0.5, 0.25], [0.0, 1.0])
-    @test_throws DimensionMismatch Resource([0.5, 0.5], [0.0])
-
-    @test length(Resource([1], [0])) == 1
-    @test length(Resource([0.5, 0.25, 0.25], [0, 1, 2])) == 3
-
-    @test size(Resource([1], [0])) == (1,)
-    @test size(Resource([0.5, 0.25, 0.25], [0, 1, 2])) == (3,)
-
-    let r = Resource([0.1, 0.9], [1, 1e-1])
-        @test r.p ≈ [0.1, 0.9]
-        @test r.H ≈ [1, 1e-1]
-    end
-
-    let r = Resource([0.1, 0.5, 0.4], [1, 1e-1, 1e-2])
-        @test r.p ≈ [0.1, 0.5, 0.4]
-        @test r.H ≈ [1, 1e-1, 1e-2]
-    end
-
-    let r = Resource(2.0, [1, 10, 100])
-        @test r.p ≈ [e^-2, e^-20, e^-200] / (e^-2 + e^-20 + e^-200)
-        @test r.H ≈ [1, 10, 100]
-    end
-
-    let r = Resource(2.0, [1, 1, 2])
-        @test r.p ≈ [e^2, e^2, 1] / (1 + 2e^2)
-        @test r.H ≈ [1, 1, 2]
-    end
-
-    let r = Resource(1, [1,2])
-        @test r.p ≈ [1.0, 0.0]
-        @test r.H ≈ [1, 2]
-    end
-
-    let r = Resource(3, [2, 3, 3, 4])
-        @test r.p ≈ [0.0, 0.0, 1.0, 0.0]
-        @test r.H ≈ [2, 3, 3, 4]
-    end
-end
-
 @testset "Subspace" begin
     @test_throws ArgumentError StateSpace([0], 1, [0.1, 1], 1)
     @test_throws ArgumentError StateSpace([1,2], -1, [0.1, 1], 1)
